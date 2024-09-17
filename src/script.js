71199c109505c4a8434a6ee016570abc1df45e47
rtwoo <rtwoo@asu.edu>
2024-09-16 18:24:31 -0700

    Move JS to after HTML content, removed separate script.js file

--- src/script.js
+++ src/script.js
@@ -1,14 +1,2 @@
-var typed = new Typed("#text", {
-    strings: [
-      "Student",
-      "Software Developer",
-      "Web Developer",
-      "Programmer",
-      "Learner",
-    ],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true,
-  });
+
 
